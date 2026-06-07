63e41d13fe4882e6568b63981bf2c9d81cebe9ac
ying <ying@ying>
2026-06-07 12:30:11 +0700

    Initial Commit.

---
+++ dlt/src/main/java/dlt/DltApplication.java
@@ -0,0 +1,13 @@
+package dlt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DltApplication.class, args);
+	}
+
+}
---
+++ dlt/src/test/java/dlt/DltApplicationTests.java
@@ -0,0 +1,13 @@
+package dlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
